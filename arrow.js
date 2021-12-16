f8b2e3d35235952737f26f63412f689fa39a19a0
poojithaunnam <90605931+poojithaunnam@users.noreply.github.com>
2021-12-16 16:22:27 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,16 @@
+//function abc(){
+  //  console.log("hi");
+//}
+//abc();
+
+//()=>{console.log('welcome')}
+
+var x=(a,b)=>a+b
+console.log(x(10,20));
+
+var xyz=(x,y)=> {
+    console.log('the result is ${x+y}')
+    return x+y;
+}
+
+console.log(xyz(10,20));
