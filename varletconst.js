4a6fbba34894c5a95e29e37185a5d36d725fcf20
poojithaunnam <90605931+poojithaunnam@users.noreply.github.com>
2021-12-16 14:54:40 +0530

    Add files via upload

---
+++ varletconst.js
@@ -0,0 +1,15 @@
+function abc(){
+    var fruits=["apple","orange","grapes","banana"]
+for (var index =0;index<fruits.length;index++){
+    var element=fruits[index];
+    console.log(element);
+
+
+}
+console.log(index);
+console.log(element);
+console.log(fruits);
+
+}
+
+abc();
