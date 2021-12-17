7ac721fd7d18ff1c8b61d04c71be82d7c9dd059a
poojithaunnam <90605931+poojithaunnam@users.noreply.github.com>
2021-12-17 15:26:21 +0530

    Add files via upload

---
+++ generics.js
@@ -0,0 +1,4 @@
+var arr = ['apple', 1, 2, , true, false, function () { return 'hello'; }, 10, 12];
+console.log(arr);
+var arr1 = [1, 2, 3, 4, 5, 6];
+console.log(arr1);
---
+++ generics.ts
@@ -0,0 +1,10 @@
+let arr=['apple',1,2,true,false,()=>{return 'hello'},10,12];
+
+console.log(arr);
+
+let arr1:Array<number>=[1,2,3,4,5,6];
+console.log(arr1);
+
+for (const i of arr1) {
+    console.log(i);
+}
